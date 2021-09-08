13b31ba7877d160f9587d2fe534eb0e5cd49f297
Caspian Maclean <30039802@tafe.wa.edu.au>
2021-09-08 10:50:40 +0800

    Print example table, a 2d array of String.

--- src/javareadcsvdemo/JavaReadCSVDemo.java
+++ src/javareadcsvdemo/JavaReadCSVDemo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javareadcsvdemo;
 
 /**
@@ -18,11 +13,32 @@ package javareadcsvdemo;
  */
 public class JavaReadCSVDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // To do: print example data from array.
+        // Then read from CSV file.
+        // Then display in GUI.
+
+        String[][] salutation = {{"Mr. ", "Mrs. ", "Ms. "}, {"Kumar"}};
+        //Read more: https://www.java67.com/2014/10/how-to-create-and-initialize-two-dimensional-array-java-example.html#ixzz75ptfMUL8        
+
+        int width = 2;
+        int height = 3;
+
+        //not sure why this doesn't work:
+        //String[][] table = new String[height][width];
+        //table[0] = {"", ""};
+        String[][] table = {
+            {"Name", "Favourite Colour"},
+            {"John", "Green"},
+            {"Penny", "Blue"}
+        };
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                System.out.print(" | ");
+                System.out.print(table[row][column]);
+            }
+            System.out.println(" | ");
+        }
     }
 
 }
