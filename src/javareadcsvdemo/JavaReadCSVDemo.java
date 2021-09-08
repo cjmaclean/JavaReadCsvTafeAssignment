de86bd0d7b30c8a971e46a17a2da1b1335b116bb
Caspian Maclean <30039802@tafe.wa.edu.au>
2021-09-08 10:10:33 +0800

    Initial commit

---
+++ src/javareadcsvdemo/JavaReadCSVDemo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javareadcsvdemo;
+
+/**
+ *
+ * @author 30039802 Caspian Maclean
+ *
+ * Question 6 – JMC receives output from many different programs in CSV format
+ * you must write a program to display this data. You need to create a program
+ * that uses an external 3rd party library to read and write data to a CSV (as
+ * an example https://www.javatpoint.com/how-to-read-csv-file-in-java ) this
+ * data must be displayed in a GUI (a table is fine)
+ *
+ */
+public class JavaReadCSVDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
